21e02771371f286255e7c5ba93651d0fa31b4fe0
Diya Wadhwani <wadhwani2399@gmail.com>
2024-10-21 02:20:34 -0700

    Updated Queries

--- db/dmlQueries/Query3.sql
+++ db/dmlQueries/Query3.sql
@@ -1,7 +1,7 @@
 -- This query shows the number of active members in each club and includes clubs with any active members
 
-SELECT c.name, COUNT(m.studentID) AS activeMemberCount
+SELECT c.name AS clubName, COUNT(m.studentID) AS activeMemberCount
 FROM Club c
 LEFT JOIN Membership m ON c.clubID = m.clubID AND m.isActive = 'yes'
 GROUP BY c.name
-HAVING COUNT(m.studentID) > 0; 
+HAVING COUNT(m.studentID) > 0;
--- db/dmlQueries/Query5.sql
+++ db/dmlQueries/Query5.sql
@@ -2,9 +2,9 @@
 
 SELECT s.firstName, s.lastName, 
     CASE 
-        WHEN s.graduationDate < '2023-01-01' THEN 'Graduated'
+        WHEN s.graduationDate < '2023-01-01' THEN 'Alum'
         WHEN s.graduationDate BETWEEN '2023-01-01' AND '2024-12-31' THEN 'Current Student'
-        ELSE 'Future Student'
+        ELSE 'Prospective Student'
     END AS studentStatus
 FROM 
     Student s;
